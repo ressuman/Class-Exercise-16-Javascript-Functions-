592ec746340f5bc61555273282518a20780ab431
Richard Essuman <ressuman001@st.ug.edu.gh>
2023-06-07 11:55:33 +0000

    1st Commit

---
+++ Class Exercise 16.js
@@ -0,0 +1,20 @@
+function sample0(number0,number1,number2) {
+  let answer = number0+number1-number2;
+  return answer;
+}
+sample0(84,52,16)
+console.log(sample0(84,52,16));
+
+function sample1(number3,number4,number5) {
+  let answer = number3-number4-number5;
+  return answer;
+}
+sample1(8,52,160)
+console.log(sample1(8,52,160));
+
+function sample2(number6,number7,number8) {
+  let answer = number6-number7+number8;
+  return answer;
+}
+sample2(87,52,160)
+console.log(sample1(87,52,160));
